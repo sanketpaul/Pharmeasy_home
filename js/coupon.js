5faf5cd987c46de74725fb23f1b1b663f9b86a81
sanketpaul <sanketpaul7@gmail.com>
2022-09-04 09:21:37 +0530

    Updated commit

---
+++ js/coupon.js
@@ -0,0 +1,13 @@
+let loadcoupon=()=>{
+    document.getElementById('search_bar').style.opacity="0.3"
+    document.getElementById('banner_section').style.opacity="0.3";
+    document.getElementById('coupon').style.display='block'
+    document.getElementById('services1').style.opacity="0.3"
+}
+let closecoupon=()=>{
+    document.getElementById('coupon').style.display='none';
+    document.getElementById('search_bar').style.opacity="1"
+    document.getElementById('banner_section').style.opacity="1"
+    document.getElementById('services1').style.opacity="1"
+
+}
